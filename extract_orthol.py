73753948d2ab296b2d36299aca301857c3ad0360
cmdoret <cyril.mattheydoret@gmail.com>
2017-05-01 14:02:07 +0200

    simplified, optimized and commented code for extract_otholog

--- extract_orthol.py
+++ extract_orthol.py
@@ -1,61 +1,52 @@
-
-# The purpose of this script is to extract a list of genes that are orthologous  between bumblebee (B)- and honeybee (H)-specific
-# bacterial species. It takes an ortholog table as input and output a similar table containing only B-H orthologs.
+import itertools
+from os.path import join
+import pandas as pd
+# The purpose of this script is to extract a list of genes that are orthologous
+# between bumblebee (B)- and honeybee (H)-specific bacterial species. It takes
+# an ortholog table as input and output a similar table containing only B-H
+# orthologs.
 # Laurent Casini, Cyril Matthey-Doret
 # 04.04.2017
 
 #==========================================
 ## Loading data
+files = {}  # Data structure containing output files
+groups = ['B','H','O']
+# Three groups considered here: Bumblebee , Honeybee, Outgroup
+for L in range(1,len(groups)+1):
+    for combi in itertools.combinations(groups,L):
+        # Need all combinations of 1, 2 or all 3 of these groups
+        combi = ''.join(combi)  # Tuple output concatenated to string
+        files[combi] = open(join("data","gene_sets",combi + "_genes.txt"),'w')
+        # Each file is a dictionary value with the group as key (e.g. "BH").
 
-# List of genomes IDs
-ID_H = ['JF72','F259','JG30','JF76','F260','F261','F262','F263',
-        'JF73','JF74','JF75','WB8','WB10','L185','L186',
-        'L184','L183'] # Honeybee-bacterial genomes
-ID_B = ['F225','F230','F233','F234','F236','F237',
-        'F228','F245','F246','F247'] # Bumblebee-bacterial genomes
-ID_O = ['LA14','LA2','LDB','LGAS','LHV','LJP','WANG','JG29'] # Outgroup bacterial genomes
+
+gnm = pd.read_csv(join("data","strain_list"),sep="\t")
+# Contains genomes ID's
+ID_H = list(gnm[gnm.host_group=="H"].OrthoMCL_prefix)
+# Honeybee-bacterial genomes
+ID_B = list(gnm[gnm.host_group=="B"].OrthoMCL_prefix)
+# Bumblebee-bacterial genomes
+ID_O = list(gnm[gnm.host_group=="O"].OrthoMCL_prefix)
+# Outgroup bacterial genomes
 
 # Loading ortholog table and creating output files
 ortho_tab = open("./data/mclOutput",'r')
-B_genes = open("./data/gene_sets/B_genes.txt",'w')
-H_genes = open("./data/gene_sets/H_genes.txt",'w')
-HO_genes = open("./data/gene_sets/HO_genes.txt",'w')
-BH_genes = open("./data/gene_sets/BH_genes.txt",'w')
-BO_genes = open("./data/gene_sets/BO_genes.txt",'w')
-BHO_genes = open("./data/gene_sets/BHO_genes.txt",'w')
-O_genes = open("./data/gene_sets/O_genes.txt",'w')
 #==========================================
 
 for line in ortho_tab:  # Each line is a gene family
     tmp = line.split("\t")  # split line by word (tab separated)
-    B = False; H = False; O = False  # Is this family present in bumble/honeybees
+    comb_group = set()  # Is this family present in bumble/honeybees/outgroup
     for word in tmp: # For each word (a word is: "strain|gene")
         gene = word.split("|")  # sparates strain from gene in a list
 
-        if gene[0] in ID_H: # if the strain is in honeybee
-            H = True
         if gene[0] in ID_B: # if the strain is in bumblebee
-            B = True
+            comb_group.add("B")
+        if gene[0] in ID_H: # if the strain is in honeybee
+            comb_group.add("H")
         if gene[0] in ID_O: # if the strain is in outgroup
-            O = True
-
-    if B and H and O:  # if gene family in all strains
-        BHO_genes.write(line)  # writing family to all file
-    elif B and H and not O:
-        BH_genes.write(line)
-    elif B and not H and O:
-        BO_genes.write(line)
-    elif B and not H and not O:
-        B_genes.write(line)
-    elif not B and H and O:
-        HO_genes.write(line)
-    elif not B and H and not O:
-        H_genes.write(line)
-    elif not B and not H and O:
-        O_genes.write(line)
-
-
-
-ortho_tab.close();BHO_genes.close()
-B_genes.close();BH_genes.close();BO_genes.close()
-H_genes.close();HO_genes.close();O_genes.close()
+            comb_group.add("O")
+    comb_group = ''.join(sorted(comb_group))  # compressing set to a string
+    files[comb_group].write(line)  # Writing line to file matching group
+ortho_tab.close()  # closing input file
+for x in files: files[x].close()  # closing all output files in a loop
