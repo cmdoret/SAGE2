73ad8d3971cc474d74d5c4ebbc918a5e7b2a9865
cmdoret <cyril.mattheydoret@gmail.com>
2017-05-20 11:49:36 +0200

    updated eggNOG_parser.py to compare host group vs OTHER strains, and not all strains (including itself).

--- scripts/eggNOG_parser.py
+++ scripts/eggNOG_parser.py
@@ -1,5 +1,10 @@
-# This script extracts the GO terms from the eggNOG annotation output and counts
-# produces a list of GO terms with their frequency in a given host group.
+# This script extracts the GO terms from the eggNOG annotation output, counts
+# the number of occurences of each term and performs a GO term enrichment test
+# for a given host group (host group input file given as command line argument).
+# The enrichment test uses the Fisher exact test and p-values are corrected for
+# multiple testing using the Benjamini-Hochberg procedure. The script finally
+# writes a list of significantly enriched (q-value <= 0.05 ) GO terms to a file,
+# with their GO informations, oddratios, p-values and q-values.
 # Cyril Matthey-Doret
 # 18.05.2017
 
@@ -10,8 +15,8 @@ import MySQLdb  # Sending SQL queries to GO consortium database
 from os.path import basename  # Allows to remove path from filename
 from numpy import median  # fast vectorized median method
 from copy import copy  # copy data structures
-from scipy.stats import fisher_exact
-import numpy as np
+from scipy.stats import fisher_exact  # Used for enrichment test
+import numpy as np  # Fast data structures and vectorized methods
 
 
 """ Loading data """
@@ -19,12 +24,14 @@ import numpy as np
 # group_file = open('../data/gene_sets/BH_genes.txt','r')
 group_file = open(argv[1],'r')  # OrthoMCL table for input host group
 group_content = group_file.read()  # Reading whole file as a string
+
+# Splitting strain|gene doublets
 group_content = re.split(r'[\n\t]',group_content)
 group_content = filter(None, group_content)
-# Splitting strain|gene doublets
+# In case there are consecutive tabs/newlines, empty items are removed
 
 eggNOG = pd.read_csv('data/eggNOG_all_annotations',sep='\t',header=None)
-# Reading eggNOG annotations (concatenated from all strains)
+# Reading eggNOG annotations (concatenated from all other strains)
 strains = pd.read_csv('data/strain_list',sep='\t')
 # Strain list for ID conversions
 
@@ -32,13 +39,16 @@ strains = pd.read_csv('data/strain_list',sep='\t')
 
 GO = eggNOG[[0,5]]  # Only keeping GO terms and corresponding gene ID
 
-GO_set = copy(GO).dropna()  # Duplicating table and removing NAs
 GO_group = GO[GO[0].isin(group_content)].dropna()
 # Keeping only Genes that are in orthoMCL table of current host group
 
+GO_set = copy(GO).dropna()  # For all strains Duplicating table and removing NAs
+# Excluding rows in host group to keep only all other strains (not in host group)
+GO_set = GO_set.drop(GO_group.index)
+
 # Splitting GO terms into a list for each gene
 GO_group[5] = GO_group[5].str.split(',')  # GO terms in host group
-GO_set[5] = GO_set[5].str.split(',')  # GO terms in all strains
+GO_set[5] = GO_set[5].str.split(',')  # GO terms in all other strains
 
 def GO_expand(go, go_dict):
     """
@@ -51,13 +61,13 @@ def GO_expand(go, go_dict):
     try:
         go_dict[go] += 1  # If term already in dictionary, increment occurences
     except KeyError:
-        go_dict[go] = 0  # If absent, add it as a new key
+        go_dict[go] = 1  # If absent, add it as a new key
 
-GO_occ, GO_all = {},{}  # Inializing dictionaries for host group and all strains
+GO_occ, GO_all = {},{}  # Inializing dict. for host group and all other strains
 GO_group[5].map(lambda x: map(lambda y: GO_expand(y,GO_occ),x))  # host group
-GO_set[5].map(lambda x: map(lambda y: GO_expand(y,GO_all),x))  # all strains
+GO_set[5].map(lambda x: map(lambda y: GO_expand(y,GO_all),x))  # other strains
 # Using nested maps to apply 'GO_expand' on each GO term.
-# The first map will return each cell of column 5 and the second map will apply
+# The first map returns each cell of column '5' and the second map will apply
 # the function of every element of the list in each cell.
 
 """
@@ -70,27 +80,33 @@ table:
 GO: Gene Ontology term
 n: not
 h: host
-a: all
+a: all other
 
-Question: Is a GO term more frequent in the 'host' group than in 'all' strains?
-general concept: if Goh/nGOh >> GOa/nGOa -> GO enriched in group
-implementation: fisher_exact([[GOh,GOa],[nGOh,nGOa]])
+Question:
+    Is a GO term more frequent in the 'host' group than in 'all other' strains?
+general concept:
+    if Goh/nGOh >> GOa/nGOa -> GO enriched in host group
+implementation:
+    fisher_exact([[GOh,GOa],[nGOh,nGOa]])
 """
 
 # Building dataframe with all GO terms found in host group.
 # GOh: number of occurences of each GO term in host group
-# GOa: number of occurences of each GO term in all strains
+# GOa: number of occurences of corresponding GO terms in all other strains
+
 GO_calc = pd.Series(GO_occ).to_frame(name='GOh').merge(
     pd.Series(GO_all).to_frame('GOa'),how='left',right_index=True,
     left_index=True)
 
-# Storing total number of GO terms occuring in both host group and all strains
+GO_calc.GOa = GO_calc.GOa.fillna(value=0)  # Replacing NA's with 0's
+
+# Storing total number of GO terms occuring in host group and all other strains
 tot_GO = {'host':sum(GO_occ.itervalues()),
           'all':sum(GO_all.itervalues())}
 
 # adding column for all occurences of other GO terms in:
 GO_calc['nGOh'] = GO_calc.GOh.apply(lambda x: tot_GO['host'] - x)  # host group
-GO_calc['nGOa'] = GO_calc.GOa.apply(lambda x: tot_GO['all'] - x)  # all strains
+GO_calc['nGOa'] = GO_calc.GOa.apply(lambda x: tot_GO['all'] - x)  # all other
 
 def Fisher_row(r):
     """
@@ -121,6 +137,7 @@ def p_adjust_bh(p):
     return q[by_orig]
 
 GO_calc['Fisher_out'] = GO_calc.apply(Fisher_row,axis=1)
+
 # Use exact Fisher test to measure enrichment
 GO_enrich=pd.DataFrame({'oddratios':GO_calc['Fisher_out'].apply(lambda r: r[0]),
                           'pval':GO_calc['Fisher_out'].apply(lambda r: r[1])})
@@ -149,7 +166,7 @@ full_annot = sql_annot.merge(GO_enrich, left_on='acc',
                              right_index=True,how='inner')
 # Merging frequency and annotations into single dataframe
 
-top_GO = full_annot.loc[(full_annot['qval']<np.float(0.05))]
+top_GO = full_annot.loc[(full_annot['qval']<=np.float(0.05))]
 # Including only annotations with q-values below cutoff
 
 host_group = basename(argv[1]).split('_')[0]
